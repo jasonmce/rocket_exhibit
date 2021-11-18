717a72314d5c575017e81468bfcb71adf9aaf3d6
McEachen, Jason <mceachenj@hhmi.org>
2021-11-17 22:28:57 -0500

    Updated autostart settings to disable screensaver screen blanking

--- set_boot_to_browser.sh
+++ set_boot_to_browser.sh
@@ -1,4 +1,10 @@
 # Add our chromium-browser statement to autostart if it is not already there.
 if !grep -ri chromium-browser /etc/xdg/lxsession/LXDE-pi/autostart then
-  cat "/usr/bin/chromium-browser --kiosk  --disable-restore-session-state http://localhost:3000" >> /etc/xdg/lxsession/LXDE-pi/autostart
+  cat """
+  # Disable screen saver.
+  xset -dpms
+  xset s off
+  # Launch kiosk mode browser pointed at our app.
+  /usr/bin/chromium-browser --kiosk  --disable-restore-session-state http://localhost:3000
+  """ >> /etc/xdg/lxsession/LXDE-pi/autostart
 fi
